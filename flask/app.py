b666d7a2457888ba31bc8cf3015969b797f7713c
Melanie Roberts <robertsmelaniecc@gmail.com>
2023-06-02 21:15:13 -0400

    same error

--- flask/app.py
+++ flask/app.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template, request, send_file
 import cv2
 import numpy as np
 import io
-from nltk.tokenize import sent_tokenize
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['WTF_CSRF_ENABLED'] = False
 
 @app.route('/')
@@ -15,32 +14,84 @@ def index():
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    # Get the uploaded image file
-    file = request.files['image']
+    if request.method == 'POST':
+        # Get the uploaded image file
+        file = request.files['image']
 
-    # Read the image using OpenCV
-    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
+        # Read the image using OpenCV
+        image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
 
-    # Perform image processing and conversion
-    # ... Implement your own logic here ...
-    # Convert the image to black and white outline
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+        # Perform image processing and conversion
+        # ... Implement your own logic here ...
+        # Convert the image to black and white outline
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
-    # Create a byte buffer to store the converted image
-    buffer = io.BytesIO()
+        # Create a byte buffer to store the converted image
+        buffer = io.BytesIO()
 
-    # Save the converted image to the buffer in PNG format
-    cv2.imwrite(buffer, threshold, format='PNG')
+        # Save the converted image to the buffer in PNG format
+        cv2.imwrite(buffer, threshold, format='PNG')
 
-    # Set the buffer position to the start
-    buffer.seek(0)
+        # Set the buffer position to the start
+        buffer.seek(0)
 
-    # Return the converted image as a response
-    return send_file(buffer, mimetype='image/png')
+        # Return the converted image as a response
+        return send_file(buffer, mimetype='image/png')
+
+    # If the request method is not POST, return an error
+    return "Method Not Allowed", 405
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# from flask import Flask, render_template, request, send_file
+# import cv2
+# import numpy as np
+# import io
+# from nltk.tokenize import sent_tokenize
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+# CORS(app)
+
+# app.config['WTF_CSRF_ENABLED'] = False
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/convert', methods=['POST'])
+# def convert():
+#     # Get the uploaded image file
+#     file = request.files['image']
+
+#     # Read the image using OpenCV
+#     image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
+
+#     # Perform image processing and conversion
+#     # ... Implement your own logic here ...
+#     # Convert the image to black and white outline
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+#     # Create a byte buffer to store the converted image
+#     buffer = io.BytesIO()
+
+#     # Save the converted image to the buffer in PNG format
+#     cv2.imwrite(buffer, threshold, format='PNG')
+
+#     # Set the buffer position to the start
+#     buffer.seek(0)
+
+#     # Return the converted image as a response
+#     return send_file(buffer, mimetype='image/png')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 #
 # from nltk.tokenize import sent_tokenize
 # from sklearn.feature_extraction.text import CountVectorizer
--- flask/templates/script.js
+++ flask/templates/script.js
@@ -1,12 +1,12 @@
 // Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Get the form and input element
-  const form = document.getElementById('uploadForm');
-  const imageInput = document.getElementById('imageInput');
-  const previewContainer = document.getElementById('preview');
+  const form = document.getElementById("uploadForm");
+  const imageInput = document.getElementById("imageInput");
+  const previewContainer = document.getElementById("preview");
 
   // Listen for form submission
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     // Get the selected image file
@@ -16,41 +16,94 @@ document.addEventListener('DOMContentLoaded', () => {
     if (file) {
       // Create a FormData object to send the file to the server
       const formData = new FormData();
-      formData.append('image', file);
+      formData.append("image", file);
 
       // Send the image file to the server for processing
-
-    //# ...
-
-      fetch('/convert', {
-        method: 'POST',
-        body: formData
+      fetch("/convert", {
+        method: "POST",
+        body: formData,
       })
-      .then(response => response.blob())
-      .then(blob => {
-        // Create a URL for the processed image blob
-        const imageURL = URL.createObjectURL(blob);
+        .then((response) => response.blob())
+        .then((blob) => {
+          // Create a URL for the processed image blob
+          const imageURL = URL.createObjectURL(blob);
 
-        // Display the converted image
-        const image = new Image();
-        image.src = imageURL;
-        previewContainer.innerHTML = '';
-        previewContainer.appendChild(image);
+          // Display the converted image
+          const image = new Image();
+          image.src = imageURL;
+          previewContainer.innerHTML = "";
+          previewContainer.appendChild(image);
 
-        // Enable print button
-        const printButton = document.createElement('button');
-        printButton.innerText = 'Print';
-        printButton.addEventListener('click', () => {
-          window.print();
+          // Enable print button
+          const printButton = document.createElement("button");
+          printButton.innerText = "Print";
+          printButton.addEventListener("click", () => {
+            window.print();
+          });
+          previewContainer.appendChild(printButton);
+        })
+        .catch((error) => {
+          console.error("An error occurred:", error);
         });
-        previewContainer.appendChild(printButton);
-      })
-      .catch(error => {
-        console.error('An error occurred:', error);
-      });
     } else {
       // If no file was selected, display an error message
-      console.error('Please select an image file.');
+      console.error("Please select an image file.");
     }
   });
 });
+// // Wait for the DOM to load
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Get the form and input element
+//   const form = document.getElementById("uploadForm");
+//   const imageInput = document.getElementById("imageInput");
+//   const previewContainer = document.getElementById("preview");
+
+//   // Listen for form submission
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     // Get the selected image file
+//     const file = imageInput.files[0];
+
+//     // Validate that a file was selected
+//     if (file) {
+//       // Create a FormData object to send the file to the server
+//       const formData = new FormData();
+//       formData.append("image", file);
+
+//       // Send the image file to the server for processing
+
+//       //# ...
+
+//       fetch("/convert", {
+//         method: "POST",
+//         body: formData,
+//       })
+//         .then((response) => response.blob())
+//         .then((blob) => {
+//           // Create a URL for the processed image blob
+//           const imageURL = URL.createObjectURL(blob);
+
+//           // Display the converted image
+//           const image = new Image();
+//           image.src = imageURL;
+//           previewContainer.innerHTML = "";
+//           previewContainer.appendChild(image);
+
+//           // Enable print button
+//           const printButton = document.createElement("button");
+//           printButton.innerText = "Print";
+//           printButton.addEventListener("click", () => {
+//             window.print();
+//           });
+//           previewContainer.appendChild(printButton);
+//         })
+//         .catch((error) => {
+//           console.error("An error occurred:", error);
+//         });
+//     } else {
+//       // If no file was selected, display an error message
+//       console.error("Please select an image file.");
+//     }
+//   });
+// });
